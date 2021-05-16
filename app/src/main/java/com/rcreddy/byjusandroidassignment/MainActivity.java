7782a12cf24a2c631cb03a39007426cbf3120fb2
RAMCHANDRA REDDY <rcreddy484@gmail.com>
2021-05-16 22:56:52 +0530

    Main page UI and ServiceCall setUp

--- app/src/main/java/com/rcreddy/byjusandroidassignment/MainActivity.java
+++ app/src/main/java/com/rcreddy/byjusandroidassignment/MainActivity.java
@@ -3,12 +3,55 @@ package com.rcreddy.byjusandroidassignment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.list_view);
+        getNewsHeadLines();
     }
+
+    void getNewsHeadLines(){
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=API_KEY";
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+// Add the request to the RequestQueue.
+        queue.add(stringRequest);
+    }
+
 }
